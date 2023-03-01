9cc3bb5ffcb51e056f05765d5274e40857f05a77
avinash <bagde.avinash66@gmail.com>
2023-03-01 18:53:27 +0530

    new changes added in fill form changes for datatype PNG

--- src/App.js
+++ src/App.js
@@ -1,47 +1,97 @@
 import "./input.css";
 import SignatureCanvas from "react-signature-canvas";
-import { PDFDocument } from "pdf-lib";
 import {Button, Container, Paper, TextField} from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import {useRef, useState} from "react";
+import { PDFDocument } from 'pdf-lib';
+import {baseEncodedString} from "./baseEncodedString";
+import {btoa} from "buffer";
+
 
 function App() {
     const [saveSignature, setSaveSignature] = useState(null);
     const [userName, setUserName] = useState("");
-    const clearTextPad = useRef(null);
-    const clearSignaturePad = () => clearTextPad.current.clear();
+    const signaturePad = useRef(null);
+
+    const clearSignaturePad = () => {
+        signaturePad.current.clear();
+        setSaveSignature(null);
+    };
+    function Download(arrayBuffer, type) {
+        var blob = new Blob([arrayBuffer], { type: type });
+        var url = URL.createObjectURL(blob);
+        window.open(url);
+    }
+
+    function _base64ToArrayBuffer(base64) {
+        var binary_string = window.btoa(base64);
+        var len = binary_string.length;
+        var bytes = new Uint8Array(len);
+        for (var i = 0; i < len; i++) {
+            bytes[i] = binary_string.charCodeAt(i);
+        }
+        return bytes.buffer;
+    }
     const fillForm = async () => {
-        const formUrl = "/Referral_Aggrement_fillable.pdf";
-        const formPdfBytes = await fetch(formUrl).then((res) => res.arrayBuffer());
-        console.log(formPdfBytes);
+        setSaveSignature(_base64ToArrayBuffer(signaturePad.current.toDataURL('image/png')));
+        // converting the pdf in base encoded string and loading the document in PDFDocument.load call from pdf-lib
+        // const formPdfBytes = await fetch(formUrl).then((res) => res.arrayBuffer());
         // Load a PDF with form fields
-        const pdfDoc = await PDFDocument.load(formPdfBytes);
+
+
+
+
+        const pdfDoc = await PDFDocument.load(baseEncodedString);
+        const pngImage = await pdfDoc.embedPng(saveSignature);
+        console.log('pngImage - - -', pngImage);
+        const jpgDims = pngImage.scale(0.25);
+
         // Get the form containing all the fields
         const form = pdfDoc.getForm();
-        console.log(pdfDoc);
+        const pages = pdfDoc.getPages();
+        const firstPage = pages[7];
+        firstPage.drawImage(pngImage, {
+            x: firstPage.getWidth() / 1 - jpgDims.width / 1,
+            y: firstPage.getHeight() / 1 - jpgDims.height / 1,
+            width: jpgDims.width,
+            height: jpgDims.height,
+        });
+
+
+
         // // Get all fields in the PDF by their names
         const dayField = form.getTextField("day");
+        dayField.enableReadOnly();
         const monthField = form.getTextField("month");
+        monthField.enableReadOnly();
         const placeField = form.getTextField("place");
+        placeField.enableReadOnly();
         const nameField = form.getTextField("name");
+        nameField.enableReadOnly();
         const title = form.getTextField("title");
+        title.enableReadOnly();
         const date = form.getTextField("date");
+        date.enableReadOnly();
+        const characterImageField = form.getSignature('Initials-mJpseoLNbq');
+        characterImageField.enableReadOnly();
 
         // // Fill in the basic info fields
         dayField.setText("day");
         monthField.setText("month");
         placeField.setText("place");
-        nameField.setText("name");
+        nameField.setText(userName);
         title.setText("title");
         date.setText("date");
 
         // // Serialize the PDFDocument to bytes (a Uint8Array)
         const pdfBytes = await pdfDoc.save();
-
+        console.log('pdfBytes- - - ',pdfBytes);
         // // Trigger the browser to download the PDF document
-        window.location.download(pdfBytes, "pdf-lib_form_creation_example.pdf", "application/pdf");
+        // window.open(pdfBytes, "pdf-lib_form_creation_example.pdf", "application/pdf");
+        Download(pdfBytes, "application/pdf");
     }
-    console.log("check new user name", userName);
+
+
     return (
     <Container maxWidth="xs">
         <br/>
@@ -57,7 +107,7 @@ function App() {
               <SignatureCanvas
                   penColor="black"
                   canvasProps={{ width: 400, height: 200, className: "sigCanvas" }}
-                  ref={clearTextPad}
+                  ref={signaturePad}
               />
           </Paper>
         </Grid>
